9b83a72d6622569cd369819ece0985c55c3a9511
Roy <114213765+RoyHgstrm@users.noreply.github.com>
2025-10-03 15:32:49 +0300

    Fix: Update CSP to allow vercel.live and clerk.repur.fi

--- next.config.js
+++ next.config.js
@@ -30,12 +30,12 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self' https://*.clerk.com;
-              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.stripe.com https://checkout.stripe.com https://r.stripe.com https://tidy-bat-20.clerk.accounts.dev https://*.clerk.com https://clerk.repur.fi blob:;
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.stripe.com https://checkout.stripe.com https://r.stripe.com https://tidy-bat-20.clerk.accounts.dev https://*.clerk.com https://clerk.repur.fi https://vercel.live blob:;
               worker-src 'self' blob:;
               style-src 'self' 'unsafe-inline';
               img-src 'self' data: https://q.stripe.com https://js.stripe.com https://stripe-camo.global.ssl.fastly.net https://d1wqzb5bdbcre6.cloudfront.net https://qr.stripe.com https://b.stripecdn.com https://files.stripe.com https://wukkulrwjqqxcxiwnflc.supabase.co https://img.clerk.com;
               media-src 'none';
-              connect-src 'self' https://api.clerk.com https://r.stripe.com https://tidy-bat-20.clerk.accounts.dev https://clerk-telemetry.com https://*.clerk.com https://accounts.repur.fi;
+              connect-src 'self' https://api.clerk.com https://r.stripe.com https://tidy-bat-20.clerk.accounts.dev https://clerk-telemetry.com https://*.clerk.com https://accounts.repur.fi https://clerk.repur.fi;
               frame-src https://js.stripe.com https://checkout.stripe.com https://*.clerk.com;
               font-src 'self' https://*.clerk.com;
             `.replace(/\n/g, '').replace(/\s{2,}/g, ' ').trim(),
